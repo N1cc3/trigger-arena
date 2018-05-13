e86b68d8ae3d2259cc89aaeb838eb443c794a4f5
Niclas Lindgren <niclas.lindgren@aalto.fi>
2018-05-14 00:56:34 +0300

    Combined react app with socket IO test.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      endpoint: "http://localhost:4001",
+      color: 'white'
+    }
+  }
+
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('change color', this.state.color) // change 'red' to this.state.color
+  }
+
+  setColor = (color) => {
+    this.setState({ color })
+  }
+
   render() {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.on('change color', (col) => {
+      document.body.style.backgroundColor = col
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send() }>Change Color</button>
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
       </div>
-    );
+    )
   }
 }
-
 export default App;
