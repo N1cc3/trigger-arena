93396e413e38b2966fde7404eb30a29d21d4a7a8
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-06-17 20:53:25 +0300

    Nicer looking player name and hp.

--- src/client/Player.js
+++ src/client/Player.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import styles from './Player.css'
 import CardMini from './CardMini'
+import heart from './img/heart.png'
 
 class Player extends Component {
   render() {
     const boardCards = this.props.cards ? this.props.cards.map((card) => (
       <CardMini key={card.number} card={card}/>
     )) : null
+    const heartStyle = {
+      backgroundImage: `url(${heart})`
+    }
     return (
 			<div className={styles.player} highlight={this.props.highlight ? "true" : "false"}>
-        <div className={styles.name}>{this.props.player.name}</div>
-        <div className={styles.hp}>{this.props.player.hp}</div>
-        {boardCards}
+        <div className={styles.stats}>
+          <div className={styles.name}>{this.props.player.name}</div>
+          <div className={styles.hp} style={heartStyle}>{this.props.player.hp}</div>
+        </div>
+        <div className={styles.cards}>
+          {boardCards}
+        </div>
 			</div>
     )
   }
