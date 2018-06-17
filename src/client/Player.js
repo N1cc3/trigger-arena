4bde93889221f6ee8d479b92e8fb769260511023
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-06-17 23:45:44 +0300

    Replace hp heart with skull when player dies.

--- src/client/Player.js
+++ src/client/Player.js
@@ -3,20 +3,27 @@ import { hot } from 'react-hot-loader'
 import styles from './Player.css'
 import CardMini from './CardMini'
 import heart from './img/heart.png'
+import skull0 from './img/skull0.png'
 
 class Player extends Component {
   render() {
     const boardCards = this.props.cards ? this.props.cards.map((card) => (
       <CardMini key={card.number} card={card}/>
     )) : null
-    const heartStyle = {
-      backgroundImage: `url(${heart})`
+
+    const hp = this.props.player.hp
+    const hpStyle = {
+      backgroundImage: `url(${hp > 0 ? heart : skull0})`
     }
+    const hpElement = <div className={styles.hp} style={hpStyle}>
+      {hp > 0 ? hp : null}
+    </div>
+
     return (
 			<div className={styles.player} highlight={this.props.highlight ? "true" : "false"}>
         <div className={styles.stats}>
           <div className={styles.name}>{this.props.player.name}</div>
-          <div className={styles.hp} style={heartStyle}>{this.props.player.hp}</div>
+          {hpElement}
         </div>
         <div className={styles.cards}>
           {boardCards}
