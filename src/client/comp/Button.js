3bdf476581a73b86bfdd28c4a061f7a6f1ec80a6
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-06-20 22:05:31 +0300

    Button no longer use native button element.

--- src/client/comp/Button.js
+++ src/client/comp/Button.js
@@ -13,27 +13,38 @@ const buttonUpSound = new Howl({
   src: [buttonUpSoundSrc]
 })
 
-const buttonDown = () => {
-  buttonDownSound.play()
-}
-
-const buttonUp = () => {
-  buttonUpSound.play()
-}
-
 class Button extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pressed: false,
+    }
+    this.buttonDown = this.buttonDown.bind(this)
+    this.buttonUp = this.buttonUp.bind(this)
+  }
+
+  buttonDown() {
+    buttonDownSound.play()
+    this.setState({pressed: true})
+  }
+
+  buttonUp() {
+    buttonUpSound.play()
+    this.setState({pressed: false})
+  }
+
   render() {
     const style = {
       backgroundColor: this.props.color,
       borderColor: this.props.color,
     }
     return (
-      <button className={[styles.button, this.props.className].join(' ')} style={style}
-        onTouchStart={buttonDown} onMouseDown={buttonDown}
-        onTouchEnd={buttonUp} onMouseUp={buttonUp}
-        onClick={this.props.onClick}>
+      <div className={[styles.button, this.props.className].join(' ')} style={style}
+        onTouchStart={this.buttonDown} onMouseDown={this.buttonDown}
+        onTouchEnd={this.buttonUp} onMouseUp={this.buttonUp}
+        onClick={this.props.onClick} pressed={this.state.pressed.toString()}>
         {this.props.children}
-      </button>
+      </div>
     )
   }
 }
