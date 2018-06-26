860d6c1862c4a49963de02542636b0b92b403081
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-06-26 22:48:21 +0300

    CardAttribute uses flow.

--- src/server/CardAttribute.js
+++ src/server/CardAttribute.js
@@ -1,5 +1,17 @@
+// @flow
+
+import RVal from './RVal'
+
 class CardAttribute {
-  constructor(id, longName, shortName, variableValues) {
+  id: number
+  longName: string
+  shortName: string
+  variableValues: ?Array<RVal>
+
+  constructor(id: number,
+              longName: string,
+              shortName: string,
+              variableValues: ?Array<RVal>) {
     this.id = id
     this.longName = longName
     this.shortName = shortName
