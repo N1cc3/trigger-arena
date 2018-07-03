7e5aef63c0d0ba96507637414ca83fbb372bee62
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-07-03 22:46:33 +0300

    Name uses flow.

--- src/client/Name.js
+++ src/client/Name.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+// @flow
+
 import { hot } from 'react-hot-loader'
+import * as React from 'react'
 import styles from './Name.css'
 import Button from './comp/Button'
+import socketIOClient from 'socket.io-client'
 
-class Name extends Component {
+type Props = {
+  socket: socketIOClient,
+  playerId: number,
+  onReady: () => void,
+}
+
+type State = {
+  name: string,
+}
+
+class Name extends React.Component<Props, State> {
   constructor(props) {
     super(props)
 
@@ -11,33 +24,28 @@ class Name extends Component {
       name: '',
     }
 
-    this.socket = this.props.socket
-
-    this.socket.on('change name', (player) => {
+    this.props.socket.on('change name', (player) => {
       if (player.id === this.props.playerId) {
         this.props.onReady()
       }
     })
-
-    this.changeName = this.changeName.bind(this)
-    this.ready = this.ready.bind(this)
   }
 
-  changeName(event) {
-    const name = event.target.value
+  changeName: (SyntheticInputEvent<HTMLInputElement>) => void = (e) => {
+    const name = e.target.value
     this.setState({name: name})
   }
 
-  ready(e) {
-    e.preventDefault()
-    this.socket.emit('change name', this.state.name)
+  ready: () => void = () => {
+    this.props.socket.emit('change name', this.state.name)
   }
 
   render() {
     return (
       <form className={styles.box} onSubmit={this.ready}>
-        <input className={styles.nameInput} type="text" name="name" maxLength="10" required autoFocus autoComplete="on" placeholder="Your name"
-        onChange={this.changeName}/>
+        <input className={styles.nameInput} type="text" name="name"
+               maxLength="10" required autoFocus autoComplete="on"
+               placeholder="Your name" onChange={this.changeName}/>
         <Button className={styles.ready} color="green" onClick={this.ready}>Ready</Button>
       </form>
     )
